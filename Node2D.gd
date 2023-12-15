00bbff27c711f95632e36713e4240616b668563c
frndotmsn <frndotmsn@jofrix.com>
2023-12-15 15:10:31 +0100

    initial commit! johann hat angefangen

--- Node2D.gd
+++ Node2D.gd
@@ -3,14 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-var andereSzene = preload("res://node_2d.tscn")
-
 func ändereScene():
-	get_tree().root.add_child(andereSzene.instantiate())
+	get_tree().change_scene_to_file("res://node_2d.tscn")
